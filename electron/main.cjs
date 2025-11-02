bdb1f574a115d0cb10252d9b8e1d99c3da6b0088
rodineyw <rodineyw@yahoo.com.br>
2025-11-02 10:11:58 -0300

    feat(electron): ocultar completamente a barra de menus (autoHideMenuBar + remover ApplicationMenu)

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,10 +1,11 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
@@ -13,9 +14,14 @@ function createWindow() {
 
   const indexPath = path.join(__dirname, '..', 'dist', 'index.html');
   win.loadFile(indexPath);
+
+  // Ensure menu bar stays hidden
+  win.setMenuBarVisibility(false);
 }
 
 app.whenReady().then(() => {
+  // Remove the default application menu entirely
+  Menu.setApplicationMenu(null);
   createWindow();
 
   app.on('activate', () => {
